e6414151a4c364f69e544b6d1821b23cc31ffd8a
Avraham Shukron <avraham.shukron@gmail.com>
2015-08-26 08:51:15 +0300

    Added generic TextCodeElement

--- pyper/core/code.py
+++ pyper/core/code.py
@@ -1,3 +1,5 @@
+from StringIO import StringIO
+
 
 class CodeElement(object):
     """
@@ -9,7 +11,24 @@ class CodeElement(object):
         Emits the current code element into the supplied source file.
 
         :param source_file:
-        :type source_file: SourceFile.
+        :type source_file: pyper.core.source.SourceFile
         """
         raise NotImplementedError("%s is an Abstract class." %
                                   (self.__class__.__name__,))
+
+
+class TextCodeElement(CodeElement):
+    """
+    A basic code element for free text.
+    Used mainly for lazy testing stuff, and generally discouraged as an actual
+    code element.
+    """
+    def __init__(self, text):
+        self.text = StringIO(text)
+
+    def add_line(self, line):
+        self.text.write("\n" + line)
+
+    def emit(self, source_file):
+        source_file.write(self.text.getvalue())
+        self.text.close()
